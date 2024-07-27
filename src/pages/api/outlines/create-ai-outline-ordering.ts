6351390a924bdb21855ea85e5fec013c6c1cdb39
Pranav Ramesh <pranav.ramesh1@gmail.com>
2024-07-27 19:45:46 -0400

    Added ai outline generator func

--- src/pages/api/outlines/create-ai-outline-ordering.ts
+++ src/pages/api/outlines/create-ai-outline-ordering.ts
@@ -5,7 +5,8 @@ This handler uses GPT to generate a list of possible ordering of outline element
 import { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/lib/supabaseClient";
 import { Database } from "@/lib/types/schema";
-import { assert } from "console";
+import { openai_client } from "@/lib/openai-client";
+import { ChatCompletionMessageParam } from "openai/resources/chat/completions.mjs";
 
 type OutlineElement = Database["public"]["Tables"]["outline_elements"]["Row"];
 type YouTubeVideo = Database["public"]["Tables"]["youtube"]["Row"];
@@ -17,6 +18,107 @@ type GPTClipMetadata = {
     description: string;
   };
 };
+
+async function get_ordering_two_calls(
+  gpt_input_clips: GPTClipMetadata[]
+): Promise<{ orderings: string[][] }> {
+  const messages: ChatCompletionMessageParam[] = [
+    {
+      role: "system",
+      content:
+        "The following is a conversation with an AI assistant about possible ordering of video news clipping outline elements.",
+    },
+    {
+      role: "user",
+      content: `Given the following clips and their metadata, what are 3 possible unique orderings of the outline elements? For each ordering, explain the reasoning behind the ordering and then output the ordering as a list of the ids.\n\n${JSON.stringify(
+        gpt_input_clips
+      )}`,
+    },
+  ];
+
+  const response = (
+    await openai_client.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: messages,
+      temperature: 0.1,
+    })
+  ).choices[0].message.content;
+
+  if (response === "") {
+    throw new Error("No response from GPT");
+  }
+
+  const pre_structured_response = (
+    await openai_client.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: messages.concat(
+        { role: "assistant", content: response },
+        {
+          role: "user",
+          content:
+            'Given the reasoning above, output the orderings as the following json schema: {"orderings": [ one permutation of ids, another permutation of ids, ... ]}. Regardless of the reasoning, your response must follow this schema. Do not include Markdown or any other formatting. Each ordering must contain some permutation of all the ids. Each ordering must be unique.',
+        }
+      ),
+      temperature: 0.1,
+    })
+  ).choices[0].message.content;
+
+  if (!pre_structured_response) {
+    throw new Error("No response from GPT");
+  }
+
+  const parsedResponse = JSON.parse(pre_structured_response);
+  if (parsedResponse.orderings === undefined) {
+    throw new Error("Parsed response does not have the expected structure.");
+  }
+
+  if (!Array.isArray(parsedResponse.orderings)) {
+    throw new Error("Parsed response does not have the expected structure.");
+  }
+
+  return parsedResponse;
+}
+
+async function get_ordering_one_call(
+  gpt_input_clips: GPTClipMetadata[]
+): Promise<{ orderings: string[][] }> {
+  const messages: ChatCompletionMessageParam[] = [
+    {
+      role: "system",
+      content:
+        "The following is a conversation with an AI assistant about possible ordering of video news clipping outline elements.",
+    },
+    {
+      role: "user",
+      content: `Given the following clips and their metadata, what are 3 possible unique orderings of the outline elements? Output the orderings as the following json schema: {"orderings": [ [one permutation of ids, another permutation of ids, ...], [one permutation of ids, another permutation of ids, ...], ... ]}. Regardless of the reasoning, your response must follow this schema. Do not include Markdown or any other formatting. Each ordering must contain some permutation of all the ids. Each ordering must be unique.\n\n${JSON.stringify(
+        gpt_input_clips
+      )}`,
+    },
+  ];
+
+  const pre_structured_response = (
+    await openai_client.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: messages,
+      temperature: 0.1,
+    })
+  ).choices[0].message.content;
+
+  if (!pre_structured_response) {
+    throw new Error("No response from GPT");
+  }
+
+  const parsedResponse = JSON.parse(pre_structured_response);
+  if (parsedResponse.orderings === undefined) {
+    throw new Error("Parsed response does not have the expected structure.");
+  }
+
+  if (!Array.isArray(parsedResponse.orderings)) {
+    throw new Error("Parsed response does not have the expected structure.");
+  }
+
+  return parsedResponse;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -35,35 +137,19 @@ export default async function handler(
       .from("outline_elements")
       .select("id, video_uuid (title, description)")
       .in("id", element_ids)
-      .returns<GPTClipMetadata>();
+      .returns<GPTClipMetadata[]>();
 
     if (error) {
       throw error;
     }
 
-    // const gptclips: GPTClipMetadata[] = [];
-    // for (const element of elements) {
-    //   const { data: clip, error } = await supabase
-    //     .from("youtube")
-    //     .select("title,description")
-    //     .eq("id", element.video_uuid)
-    //     .returns<{ title: string | null; description: string | null }>()
-    //     .single();
+    // Query OpenAI GPT to generate possible ordering of outline elements
+    // const parsedResponse = await get_ordering_two_calls(gpt_input_clips);
+    const parsedResponse = await get_ordering_one_call(gpt_input_clips);
 
-    //   if (error) {
-    //     throw error;
-    //   }
-
-    //   if (!clip) {
-    //     throw new Error(`Clip with id ${element.video_uuid} not found`);
-    //   }
-
-    //   gptclips.push({ ...clip, id: element.video_uuid });
-    // }
-
-    return res.status(200).json({ gpt_input_clips });
+    return res.status(200).json({ orderings: parsedResponse.orderings });
   } catch (error) {
-    console.error("Error fetching outline elements:", error);
-    res.status(500).json({ error: "Failed to fetch outline elements" });
+    console.error("Error generating AI outline ordering:", error);
+    res.status(500).json({ error: "Error generating AI outline ordering" });
   }
 }

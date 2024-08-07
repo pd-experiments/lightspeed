7591343e6448790c60ced56be560577d80546e1d
Dinesh Vasireddy <dineshteja@gmail.com>
2024-08-07 02:37:48 -0400

    json conversion of script + frontend overhaul on script display

--- src/app/outline/[id]/script/page.tsx
+++ src/app/outline/[id]/script/page.tsx
@@ -13,16 +13,38 @@ import { formatDuration, calculateOutlineDuration } from '@/lib/helperUtils/outl
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { Spinner } from '@/components/ui/Spinner';
+import { FaYoutube } from 'react-icons/fa';
+import { Mic, Eye, Type, Music, Film, MonitorCheck } from 'lucide-react';
 
 export default function ScriptPage({ params, searchParams }: { params: { id: string }, searchParams: { title: string } }) {
   const outlineId = params?.id as string;
   const outlineTitle = searchParams?.title || 'Untitled Outline';
-  const [fullScript, setFullScript] = useState<string>('');
+  const [fullScript, setFullScript] = useState<any[]>([]);
+
   const [loading, setLoading] = useState<boolean>(true);
   const [elementCount, setElementCount] = useState<number>(0);
   const [totalDuration, setTotalDuration] = useState<number>(0);
   const [outline, setOutline] = useState<any>(null);
   const [scriptGenerationProgress, setScriptGenerationProgress] = useState<number>(0);
+  const [videoInfo, setVideoInfo] = useState<Record<string, { title: string, id: string, video_id: string }>>({});
+  
+  const fetchVideoInfo = async (ids: string[]) => {
+    const { data, error } = await supabase
+      .from('youtube')
+      .select('id, title, video_id')
+      .in('id', ids);
+  
+    if (error) {
+      console.error('Error fetching video info:', error);
+      return;
+    }
+  
+    const newVideoInfo: Record<string, { title: string, id: string, video_id: string }> = {};
+    data.forEach(item => {
+      newVideoInfo[item.id] = { title: item.title || '', id: item.id, video_id: item.video_id || '' };
+    });
+    setVideoInfo(newVideoInfo);
+  };
 
   useEffect(() => {
     async function fetchOutlineData() {
@@ -36,19 +58,21 @@ export default function ScriptPage({ params, searchParams }: { params: { id: str
   
         if (outlineError) throw outlineError;
   
-        setFullScript(outlineData.full_script || '');
+        setFullScript(outlineData.full_script);
   
         const { data: elementsData, error: elementsError } = await supabase
           .from('outline_elements')
           .select('*')
           .eq('outline_id', outlineId);
   
+        console.log("elementsError", elementsError);
+
         if (elementsError) throw elementsError;
   
         setElementCount(elementsData.length);
         setTotalDuration(calculateOutlineDuration(elementsData));
         setOutline(outlineData);
-
+  
         setScriptGenerationProgress(outlineData.script_generation_progress);
       } catch (error) {
         console.error('Error fetching outline data:', error);
@@ -58,6 +82,16 @@ export default function ScriptPage({ params, searchParams }: { params: { id: str
     }
     fetchOutlineData();
   }, [outlineId]);
+
+  useEffect(() => {
+    if (fullScript.length > 0) {
+      const videoIds = fullScript.filter(item => item.id).map(item => item.id);
+      if (videoIds.length > 0) {
+        console.log("Fetching video info for", videoIds);
+        fetchVideoInfo(videoIds);
+      }
+    }
+  }, [fullScript]);
 
   const handleGenerateFullScript = async () => {
     try {
@@ -78,6 +112,107 @@ export default function ScriptPage({ params, searchParams }: { params: { id: str
     } finally {
       setScriptGenerationProgress(0);
     }
+  };
+
+  const renderScript = (script: any[]) => {
+    return script.map((item, index) => {
+      const renderHeader = (icon: React.ReactNode, text: string) => (
+        <h2 className="text-sm mb-2 font-semibold text-gray-600 flex items-center">
+          {icon}
+          <span className="ml-2">{text}</span>
+        </h2>
+      );
+  
+      const renderContent = (content: React.ReactNode) => (
+        <div key={index} className="mb-4 rounded-md border border-gray-200 p-3 bg-white shadow-sm">
+          {content}
+        </div>
+      );
+  
+      switch (item.type) {
+        case 'NARRATION':
+          return (
+            <div key={index}>
+              {renderHeader(<Mic className="w-4 h-4 text-blue-500" />, 'Speaking Role')}
+              {renderContent(
+                <p className="text-sm text-gray-800"><strong>{item.speaker}:</strong> {item.content}</p>
+              )}
+            </div>
+          );
+        case 'VISUAL':
+          return (
+            <div key={index}>
+              {renderHeader(<Eye className="w-4 h-4 text-green-500" />, 'Visual')}
+              {renderContent(
+                <p className="text-sm text-green-600">{item.content}</p>
+              )}
+            </div>
+          );
+        case 'TRANSITION':
+          return (
+            <div key={index}>
+              {renderHeader(<MonitorCheck className="w-4 h-4 text-purple-500" />, 'Transition')}
+              {renderContent(
+                <p className="text-sm text-purple-600">{item.content}</p>
+              )}
+            </div>
+          );
+        case 'TEXT_OVERLAY':
+          return (
+            <div key={index}>
+              {renderHeader(<Type className="w-4 h-4 text-orange-500" />, 'Text Overlay')}
+              {renderContent(
+                <p className="text-sm font-medium uppercase text-orange-600">{item.content}</p>
+              )}
+            </div>
+          );
+        case 'SOUND_EFFECT':
+          return (
+            <div key={index}>
+              {renderHeader(<Music className="w-4 h-4 text-red-500" />, 'Sound Effect')}
+              {renderContent(
+                <p className="text-sm font-medium uppercase text-red-600">{item.content}</p>
+              )}
+            </div>
+          );
+        case 'EXISTING_SCRIPT':
+        case 'SOUNDBITE':
+          const info = videoInfo[item.id];
+          const startTime = new Date(item.timestamp);
+          const endTime = new Date(startTime.getTime() + parseFloat(item.duration) * 1000);
+          
+          const formatTime = (time: Date) => {
+            return time.toISOString().substr(11, 8);
+          };
+        
+          return (
+            <div key={index}>
+              {renderHeader(<Film className="w-4 h-4 text-indigo-500" />, 'Soundbite')}
+              {renderContent(
+                <>
+                  <div className="flex items-center justify-between mb-2">
+                    <Badge variant="secondary" className="inline-flex items-center py-1 px-2 space-x-2">
+                      <Link href={`https://www.youtube.com/watch?v=${info?.video_id}`} target="_blank" rel="noopener noreferrer" className="flex items-center">
+                        <FaYoutube className="mr-1 w-4 h-4 text-red-500" />
+                        <span className="text-xs">{info?.title}</span>
+                      </Link>
+                    </Badge>
+                    {item.timestamp && (
+                      <p className="text-xs text-indigo-600">
+                        <span className="font-bold">Start:</span> {formatTime(startTime)},
+                        <span className="font-bold ml-1">End:</span> {formatTime(endTime)}
+                      </p>
+                    )}
+                  </div>
+                  <p className="text-sm text-gray-700">{item.content}</p>
+                </>
+              )}
+            </div>
+          );
+        default:
+          return <p key={index} className="text-sm text-gray-700">{item.content}</p>;
+      }
+    });
   };
 
   return (
@@ -147,7 +282,9 @@ export default function ScriptPage({ params, searchParams }: { params: { id: str
           {loading ? (
             <p>Loading...</p>
           ) : (
-            <pre className="whitespace-pre-wrap">{fullScript}</pre>
+            <div className="my-4 p-4 bg-white border border-gray-200 rounded-lg shadow">
+              <div className="space-y-4">{renderScript(fullScript)}</div>
+            </div>
           )}
         </div>
       </main>
